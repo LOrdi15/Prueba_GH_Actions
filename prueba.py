b4eb2fece67f78467404ee09a3fb4093ab8e9b9a
LOrdi15 <ordieres1508@hotmail.com>
2024-07-07 17:30:51 -0600

    Se añade txt

--- prueba.py
+++ prueba.py
@@ -9,7 +9,14 @@ def test_subtract():
 def subtract(a, b):
     return a - b
 
+
+
+def write_to_file(filename, content):
+    with open(filename, 'w') as f:
+        f.write(content)
+
 if __name__ == "__main__":
     test_sum()
     test_subtract()
     print("Prueba exitosa")
+    write_to_file('Test_Resultado.txt', 'Prueba exitosa')
